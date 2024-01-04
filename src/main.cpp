78959d5ff21227c88626b8e1a28d4a3e2668efda
Andrew Theiss <andrew.theiss@gmail.com>
2024-01-04 01:20:07 -0800

    Fix TFT pin outputs

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int myFunction(int, int);
 
 // For optimized ILI9341_t3 library
 #define TFT_DC      9
-#define TFT_CS      8
+#define TFT_CS      10
 #define TFT_RST    255  // 255 = unused, connect to 3.3V
 #define TFT_MOSI    11
 #define TFT_SCLK    13
@@ -24,16 +24,19 @@ void setup() {
   // SPI.setMOSI(7);
   // SPI.setSCK(14);
   tft.begin();
+  tft.fillScreen(tft.color565(0, 45, 255));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  tft.fillScreen(tft.color565(255, 45, 55));
   // wait for a second
   delay(5000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  tft.fillScreen(tft.color565(0, 5, 255));
    // wait for a second
   delay(1000);
 }
