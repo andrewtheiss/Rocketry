a2207577332bb26f334b4ba26ff6a753fcc62216
Andrew Theiss <andrew.theiss@gmail.com>
2024-01-04 00:43:00 -0800

    Wire up lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
+#include <ILI9341_t3.h>
 
 // put function declarations here:
 int myFunction(int, int);
+
+// For optimized ILI9341_t3 library
+#define TFT_DC      9
+#define TFT_CS      8
+#define TFT_RST    255  // 255 = unused, connect to 3.3V
+#define TFT_MOSI    11
+#define TFT_SCLK    13
+#define TFT_MISO    12
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_SCLK, TFT_MISO);
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  // SPI.setMOSI(7);
+  // SPI.setSCK(14);
+  tft.begin();
 }
 
 void loop() {
