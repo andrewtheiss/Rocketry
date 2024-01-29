44532dee1e44564a9e779ff77032796b2f8c7861
Andrew Theiss <andrew.theiss@gmail.com>
2024-01-29 01:08:01 -0800

    Update pins for BLE and 9Axis to not conflict with LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <LSMSensor.h>
-//#include <Adafruit_BluefruitLE_SPI.h>
+#include <Adafruit_BluefruitLE_SPI.h>
 
 // Setup for Bluetooth LE Friend
-#define BLUEFRUIT_SPI_CS               8
-#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_CS               7
+#define BLUEFRUIT_SPI_IRQ              6
 #define BLUEFRUIT_SPI_RST              3
 
 LSMSensor adaFruitLSM;
@@ -11,7 +11,7 @@ LSMSensor adaFruitLSM;
 void setup()
 {
   adaFruitLSM.init();
-  //Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+  Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 }
 
 void loop()
